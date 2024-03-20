9a352c9fe7d0054314d7e82853fda6e29f706871
takaeruu <yogagautama12@gmail.com>
2024-03-20 17:09:39 +0700

    login

--- app/Views/menu.php
+++ app/Views/menu.php
@@ -40,17 +40,25 @@
         </a>
         <nav class="vertnav navbar navbar-light">
           <!-- nav bar -->
-          <div class="w-100 mb-4 d-flex">
-            <a class="navbar-brand mx-auto mt-2 flex-fill text-center" href="./index.html">
-              <svg version="1.1" id="logo" class="navbar-brand-img brand-sm" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 120 120" xml:space="preserve">
-                <g>
-                  <polygon class="st0" points="78,105 15,105 24,87 87,87 	" />
-                  <polygon class="st0" points="96,69 33,69 42,51 105,51 	" />
-                  <polygon class="st0" points="78,33 15,33 24,15 87,15 	" />
-                </g>
-              </svg>
-            </a>
-          </div>
+     <div class="w-100 mb-4 d-flex">
+    <a class="navbar-brand mx-auto mt-2 flex-fill text-center" href="./index.html" style="display: flex; flex-direction: column; align-items: center;">
+        <svg version="1.1" id="logo" class="navbar-brand-img brand-sm" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 120 120" xml:space="preserve" style="margin-bottom: 10px;">
+            <g>
+                <polygon class="st0" points="78,105 15,105 24,87 87,87" />
+                <polygon class="st0" points="96,69 33,69 42,51 105,51" />
+                <polygon class="st0" points="78,33 15,33 24,15 87,15" />
+            </g>
+        </svg>
+        <span style="font-weight:bold;">Schedule Maker</span>
+         <p class="text-muted nav-heading mt-4 mb-1">
+  
+          </p>
+    </a>
+    
+</div>
+
+
+          
 
               <ul class="navbar-nav flex-fill w-100 mb-2">
             <li class="nav-item w-100">
